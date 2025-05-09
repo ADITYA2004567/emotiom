c4f5480126d713e1d5daeafb8edde89dbf485298
ADITYA2004567 <149920450+ADITYA2004567@users.noreply.github.com>
2025-05-09 12:30:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import sys
+if 'torch.classes' in sys.modules:
+    del sys.modules['torch.classes']
+
+
+import streamlit as st
+from emotion_utils import load_model, preprocess_image, predict_emotion, get_emotion_label
+
+st.set_page_config(page_title="Emotion Detection", layout="centered")
+st.title("😊 Emotion Detection from Facial Image")
+
+uploaded_file = st.file_uploader("Upload a face image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    #st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+
+    
+    model = load_model("emotion_mobilenetv2.pth")
+    input_tensor = preprocess_image(uploaded_file)
+    prediction = predict_emotion(model, input_tensor)
+    emotion_label = get_emotion_label(prediction)
+
+    st.success(f"Predicted Emotion: **{emotion_label}**")
